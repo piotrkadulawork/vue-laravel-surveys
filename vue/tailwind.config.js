c424eeb4dc73f455309768402894f6fd902d304d
Piotr Kadula <ppracakadula@gmail.com>
2022-04-02 11:20:18 +0200

    Initial commit with basic components setup vue and laravel

---
+++ vue/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  
+content: [
+  "./index.html",
+  "./src/**/*.{vue,js,ts,jsx,tsx}",
+],
+  theme: {
+    extend: {},
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
